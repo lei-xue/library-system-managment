95b70c4214dd9ff3096fcccf6bedf597f85d722b
Lei Xue <lxue48@gatech.edu>
2024-02-11 14:45:29 -0800

    Added files

---
+++ courselibrary/src/main/java/com/application/courselibrary/service/BookService.java
@@ -0,0 +1,40 @@
+package com.application.courselibrary.service;
+
+import com.application.courselibrary.entity.*;
+import com.application.courselibrary.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    public List<Book> findAllBooks(){
+        return bookRepository.findAll();
+    }
+
+    public Book findBookById(Long id) {
+        return bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+    }
+
+
+    public void createBook(Book book){
+        bookRepository.save(book);
+    }
+    public void updateBook(Book book){
+        bookRepository.save(book);
+    }
+
+
+    public void deleteBook(Long id){
+        Book book =  bookRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Book not found with id: " + id));
+
+        bookRepository.deleteById(book.getId());
+    }
+
+
+}
